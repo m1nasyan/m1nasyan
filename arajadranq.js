7b07b6218309d47f61e9caf1483a02f178df4905
m1nasyan <115705425+m1nasyan@users.noreply.github.com>
2022-10-20 10:26:28 +0400

    Update arajadranq.js

--- arajadranq.js
+++ arajadranq.js
@@ -1,9 +1,9 @@
 var result 
  
 var a= parseFloat( prompt("Enter the first number"));
-ankap = [], i = 1;
+zangvac1 = [], i = 1;
 while(i < a) {
-    if(a % i == 0) ankap.push(i);
+    if(a % i == 0) zangvac1.push(i);
     i = i + 1;
 }
 
@@ -11,28 +11,28 @@ while(i < a) {
 
 
 var b= parseFloat( prompt("Enter the second number"));
-vapsheankap = [], i = 1;
+zangvac2 = [], i = 1;
 while(i < b) {
-    if(b % i == 0) vapsheankap.push(i);
+    if(b % i == 0) zangvac2.push(i);
     i = i + 1;
 }
 
 alert("the firs nubers              " +
-    ankap + "," + a);
+    zangvac1 + "," + a);
 
 
     alert("the second nubers        " +
-    vapsheankap + "," + b); 
+    zangvac2 + "," + b); 
 
 
 
 
-    for(let x = 0;x<ankap.length;x++) {
-        for(let j=0;j<vapsheankap.length;j++){
-            if(ankap[x]==vapsheankap[j]){
+    for(let x = 0;x<zangvac1.length;x++) {
+        for(let j=0;j<zangvac2.length;j++){
+            if(zangvac1[x]==zangvac2[j]){
 
         
-        result= ankap[x]
+        result= zangvac1[x]
 
             }   
         }
